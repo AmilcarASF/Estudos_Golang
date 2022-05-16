025076ed0a96b568c78c45cc4479f3e63a0687de
AmilcarFerreira <amilcar.ferreira.easysoft.com.br>
2022-05-16 16:56:37 -0300

    Testes avançados

---
+++ testes_avancado/forma/formas_test.go
@@ -0,0 +1,28 @@
+package formas
+
+import (
+	"math"
+	"testing"
+)
+
+func TestArea(t *testing.T) {
+	t.Run("Retâgulo", func(t *testing.T) {
+		ret := Retangulo{10, 12}
+		areaEsperada := float64(120)
+		areaRecebida := ret.Area()
+
+		if areaEsperada != areaRecebida {
+			t.Fatalf("Area recebida %f é diferente da esérada %f", areaRecebida, areaEsperada)
+		}
+	})
+
+	t.Run("Círculo", func(t *testing.T) {
+		circ := Circulo{10}
+		areaEsperada := float64(math.Pi * 100)
+		areaRecebida := circ.Area()
+
+		if areaEsperada != areaRecebida {
+			t.Fatalf("Area recebida %f é diferente da esérada %f", areaRecebida, areaEsperada)
+		}
+	})
+}
